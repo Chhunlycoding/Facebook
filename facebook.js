55960df706c97af2ba222798155613c921a23996
Chhunlycoding <chhunlytb@gmail.com>
2022-12-29 22:50:24 +0700

    FaceBook

---
+++ facebook.js
@@ -0,0 +1,9 @@
+let showShare=document.querySelector('.menu-share');
+function Share(){
+    if(showShare.style.display==="none"){
+        showShare.style.display="block";
+    }
+    else{
+        showShare.style.display="none";
+    }
+}
